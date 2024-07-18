481ed3c409a5ddf76033b2c378a1043aecc83a9c
Kian Hayes <hayesk190@gmail.com>
2024-07-18 11:58:11 -0400

    hlle implosioin update

--- scripts/solvers_test_output.sh
+++ scripts/solvers_test_output.sh
@@ -3,12 +3,12 @@
 ### This script was for the automatic running of Athena++ with different solvers and equations of state AND outputs 
 ### data AND creates the plots/videos for each
 
-declare -a solvers=('hlle hllc lhllc hlld lhlld roe llf')
+declare -a solvers=('hlle hllc lhllc lhlld roe llf')
 
 PRECURSOR='[BASH SCRIPT]: '
 TEST_DIR=/data/khyaes/solvers_test
 export OMP_NUM_THREADS=16
-ALL_DATA_DIR=${TEST_DIR}/lw_collapse
+ALL_DATA_DIR=${TEST_DIR}/kh_shear
 
 for solver in $solvers
     do
@@ -23,7 +23,7 @@ for solver in $solvers
         echo
 
         cd ~/athena
-        python configure.py --prob=lw_implode -omp -hdf5 --flux=$solver --hdf5_path=$HDF5_DIR --include=/usr/include/openmpi-x86_64/ > ${ALL_DATA_DIR}/${solver}_configuration_output.txt
+        python configure.py --prob=kh -b --nscalars=1 -omp -hdf5 --flux=$solver --hdf5_path=$HDF5_DIR --include=/usr/include/openmpi-x86_64/ > ${ALL_DATA_DIR}/${solver}_configuration_output.txt
 
         if [ $? != 0 ]; then 
             echo
@@ -36,6 +36,7 @@ for solver in $solvers
             mkdir $SOLVER_DIR
             mkdir $DATA_DIR
             mkdir $PLOTS_DIR
+            mv ${ALL_DATA_DIR}/${solver}_configuration_output.txt $SOLVER_DIR
             make clean > /dev/null
             make -j 16 > ${SOLVER_DIR}/${solver}_make_output.txt
         fi
@@ -49,7 +50,7 @@ for solver in $solvers
         echo
         echo $PRECURSOR Running simulation with $solver solver in $PWD...
 
-        ~/athena/bin/athena -i /data/khyaes/solvers_test/athinput.lw_implode > ${SOLVER_DIR}/${solver}_simulation_output.txt
+        ~/athena/bin/athena -i /data/khyaes/solvers_test/athinput.kh-shear-lecoanet > ${SOLVER_DIR}/${solver}_simulation_output.txt
 
         echo
         echo $PRECURSOR Finished running simulation and printing results
@@ -57,9 +58,9 @@ for solver in $solvers
         echo
         echo $PRECURSOR Making plot images for data and rendering video...
 
-        python /home/khayes/summer2024/scripts/plot_all.py > ${SOLVER_DIR}/${solver}_plot_output.txt 2>&1
+        python /home/khayes/summer2024/scripts/plot_all_data.py > ${SOLVER_DIR}/${solver}_plot_output.txt 2>&1
 
-        ffmpeg -r 25 -i ${PLOTS_DIR}/%1d.png ../video.mp4 > ${SOLVER_DIR}/${solver}_ffmpeg_output.txt 2>&1
+        ffmpeg -r 25 -i ${PLOTS_DIR}/%1d.png ../${solver}_video.mp4 > ${SOLVER_DIR}/${solver}_ffmpeg_output.txt 2>&1
         
         echo
         echo $PRECURSOR Done with all processes for $solver solver
