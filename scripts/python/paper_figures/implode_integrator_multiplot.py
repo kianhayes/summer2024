e42df10b5d32d7b4593e5fd12163f049080e783e
Kian Hayes <hayesk190@gmail.com>
2024-08-21 11:23:35 -0400

    plots update

--- scripts/python/paper_figures/implode_integrator_multiplot.py
+++ scripts/python/paper_figures/implode_integrator_multiplot.py
@@ -6,10 +6,13 @@ integrators = ['rk2', 'rk3', 'rk4', 'vl2', '5_4']
 test_dir = "/data/khyaes/integrator_test/implode"
 
 fig = plt.figure()
+pt = 1./72.27
+golden = 16/9
+width = 513*pt
 
 grid = AxesGrid(
     fig,
-    (0.05, 0.07, 6, 2), # Change this to get the dimensions and sizing of the subplots right. It is the padding for (left, bottom, width, height)
+    (0.05, 0.07, width, width/golden), # Change this to get the dimensions and sizing of the subplots right. It is the padding for (left, bottom, width, height)
     nrows_ncols=(1, 5),
     axes_pad= 2, # Padding between the subplots
     label_mode='L',
@@ -26,13 +29,13 @@ for i, integrator in enumerate(integrators):
     s = yt.SlicePlot(ds=ds, normal='z', fields=variable)
     s.set_cmap(variable, 'inferno')
     if integrator == '5_4':
-        s.annotate_title('SSPRK5_4')
+        s.annotate_title('RK54')
     else:
         s.annotate_title(integrator.upper())
     s.set_zlim(variable, zmax=1.4e-8, zmin=7e-9)
     s.set_log(variable, False)
     s.set_colorbar_label(variable, 'Temperature')
-    s.set_font_size(58)
+    s.set_font({"size": 120})
     s.set_colorbar_minorticks(variable, False)
     s.set_minorticks(variable, False)
 
@@ -45,5 +48,5 @@ for i, integrator in enumerate(integrators):
     s.render()
 
 # 'bbox_inches='tight' here is very important for getting correct dimensions on subplots!!
-plt.savefig("/data/khyaes/integrator_test/implode/implode_integrator_multiplot.png", bbox_inches='tight', dpi=100) 
+plt.savefig("/home/khayes/summer2024/paper/plots/implode_integrator_multiplot.png", bbox_inches='tight', dpi=100) 
 
--- scripts/python/paper_figures/riemann_solution_plot.py
+++ scripts/python/paper_figures/riemann_solution_plot.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.patches as patches
 
+plt.rcParams.update({'mathtext.default':  'regular' })
+plt.rcParams['text.usetex'] = True
+
 def lines(x):
     y1 = x
     y2 = -x
@@ -22,13 +25,12 @@ plt.arrow(-10, 0, 20, 0, color='black', length_includes_head=True, head_width=0.
 plt.arrow(0, 0, 0, 10, color='black', length_includes_head=True, head_width=0.1, head_length=0.15, head_starts_at_zero=True) # Vertical 
 a = patches.FancyArrowPatch((-6.1, 6), (6.1, 6), connectionstyle="arc3,rad=-0.2")
 plt.gca().add_patch(a)
-plt.rcParams.update({'mathtext.default':  'regular' })
-plt.text(0.3, 9.8, 't', fontsize=8, fontname='Times New Roman')
-plt.text(9.8, -0.5, 'x', fontsize=8, fontname='Times New Roman')
-plt.text(3, 7, '$U^{HLL}$', fontsize=12, fontname='Times New Roman')
-plt.text(-11, 10.5, '$S_L$', fontsize=12, fontname='Times New Roman')
-plt.text(10.5, 10.5, '$S_R$', fontsize=12, fontname='Times New Roman')
-plt.text(-8.7, 1, '$U_L$', fontsize=12, fontname='Times New Roman')
-plt.text(8, 1, '$U_R$', fontsize=12, fontname='Times New Roman')
+plt.text(0.3, 9.8, 't', fontsize=8)
+plt.text(9.8, -0.5, 'x', fontsize=8)
+plt.text(3, 7, '$U^{HLL}$', fontsize=12)
+plt.text(-11, 10.5, '$S_L$', fontsize=12)
+plt.text(10.5, 10.5, '$S_R$', fontsize=12)
+plt.text(-8.7, 1, '$U_L$', fontsize=12)
+plt.text(8, 1, '$U_R$', fontsize=12)
 
 plt.savefig('E:/School/reu2024/research/summer2024/paper/plots/riemann_solution.png', bbox_inches='tight', dpi=250)
