129656b749fca79e1d4311bde06d94dee7189414
Kian Hayes <hayesk190@gmail.com>
2024-09-18 16:30:01 -0400

    presentation videos

--- scripts/python/paper_figures/processes_plot.py
+++ scripts/python/paper_figures/processes_plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-plt.rcParams['text.usetex'] = True
+plt.rcParams.update({'text.usetex': True, 'font.family': 'serif', 'font.serif': 'Computer Modern'})
 
 #data1 = pd.read_csv('/home/khayes/summer2024/athena/processes_test/processes_data1.txt', sep='\t', header=None)
 data3 = open('/data/khyaes/processes_test/test4/data/all_data.txt', 'r')
